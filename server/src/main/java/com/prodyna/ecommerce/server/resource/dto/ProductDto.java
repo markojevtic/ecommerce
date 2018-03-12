c03fa724890ea392b826ebbee892ff85d98dcec2
PRODYNA\dlukac <dragan.lukac@prodyna.com>
2018-03-12 13:23:31 +0100

    PDEC-4: Added ProductDto

--- server/src/main/java/com/prodyna/ecommerce/server/resource/dto/ProductDto.java
+++ server/src/main/java/com/prodyna/ecommerce/server/resource/dto/ProductDto.java
@@ -1,4 +1,20 @@
 package com.prodyna.ecommerce.server.resource.dto;
 
-public class ProductDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.ResourceSupport;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto extends ResourceSupport {
+
+    private String name;
+    private String description;
+    private String photoUrl;
+    private String category;
+    private boolean active;
 }

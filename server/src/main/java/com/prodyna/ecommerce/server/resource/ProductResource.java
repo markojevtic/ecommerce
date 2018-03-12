c1fe1b5046b6b5de25880c8f6dd49ed31a0f69c2
PRODYNA\dlukac <dragan.lukac@prodyna.com>
2018-03-12 10:41:59 +0100

    PDEC-4: added @RestController, @RequestMappings to ProductResource

--- server/src/main/java/com/prodyna/ecommerce/server/resource/ProductResource.java
+++ server/src/main/java/com/prodyna/ecommerce/server/resource/ProductResource.java
@@ -1,4 +1,39 @@
 package com.prodyna.ecommerce.server.resource;
 
+import com.prodyna.ecommerce.server.repository.entity.Product;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
 public class ProductResource {
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    public Product load(@PathVariable("productId") String productId) {
+
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Product insert(@RequestBody Product product) {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.PUT)
+    public Product update(@PathVariable("productId") String productId, @RequestBody Product product) {
+
+        return null;
+    }
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.DELETE)
+    public Product delete(@PathVariable("productId") String productId) {
+
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Product getAll() {
+
+        return null;
+    }
 }

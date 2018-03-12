519c24c0b6e330e394db96288d1378711c83fa63
PRODYNA\dlukac <dragan.lukac@prodyna.com>
2018-03-12 09:36:39 +0100

    PDEC-4: added fields to Product and lombok annotations

--- server/src/main/java/com/prodyna/ecommerce/server/repository/entity/Product.java
+++ server/src/main/java/com/prodyna/ecommerce/server/repository/entity/Product.java
@@ -1,4 +1,20 @@
 package com.prodyna.ecommerce.server.repository.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+
+    private String productId;
+    private String name;
+    private String description;
+    private String photoUrl;
+    private String category;
+    private boolean active;
 }

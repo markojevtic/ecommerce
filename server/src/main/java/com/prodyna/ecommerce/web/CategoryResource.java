e3a0e943b942a4f5cfdf79bd2455e2cd6d4c079f
Jelena Pajdic <jelena.pajdic@prodyna.com>
2018-03-12 23:43:18 +0100

    Category converters and rest resource

--- server/src/main/java/com/prodyna/ecommerce/web/CategoryResource.java
+++ server/src/main/java/com/prodyna/ecommerce/web/CategoryResource.java
@@ -23,7 +23,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping(value = "/category", produces = APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(value = "/categories", produces = APPLICATION_JSON_UTF8_VALUE)
 public class CategoryResource {
 
 	@Autowired

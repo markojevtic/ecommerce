beb058d07171e04169cb30614f804f0a51c60ac4
dlukac <dragan.lukac@gmail.com>
2018-04-12 14:29:06 +0200

    Added lombok annotations to User and Authorization entities

--- server/src/main/java/com/prodyna/ecommerce/server/repository/entity/User.java
+++ server/src/main/java/com/prodyna/ecommerce/server/repository/entity/User.java
@@ -1,7 +1,16 @@
 package com.prodyna.ecommerce.server.repository.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     private String username;
     private String fullName;

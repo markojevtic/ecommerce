0107c5bf2e05ac32a0ec73533c71f5d10b1b5662
dlukac <dragan.lukac@gmail.com>
2018-04-12 14:58:09 +0200

    (PDEC-5-security-draganL: Added @Id for Authorization, @Id and String password for User

--- server/src/main/java/com/prodyna/ecommerce/server/repository/entity/Authorization.java
+++ server/src/main/java/com/prodyna/ecommerce/server/repository/entity/Authorization.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Authorization {
+    @Id
+    private String authorizationId;
     private String username;
     private String password;
 }
--- server/src/main/java/com/prodyna/ecommerce/server/repository/entity/User.java
+++ server/src/main/java/com/prodyna/ecommerce/server/repository/entity/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
@@ -12,7 +13,9 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class User {
+    @Id
     private String username;
+    private String password;
     private String fullName;
     private Customer customer;
     private boolean active;
